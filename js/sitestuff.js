9ad32fe936946078e7333045f7124655649e2e4f
RoanHemmings <rorohemmings@aol.com>
2025-11-09 15:20:48 +0000

    adding folders

---
+++ js/sitestuff.js
@@ -0,0 +1,263 @@
+// sitestuff.js - This is for miscellaneous scripts that aren't one time uses, like the title generator and also important variables that effect other scripts. - Generic - (C) Roan Hemmings 2023-present
+
+// lists all variables (use _ for title urls that start with a number, e.g. 2ndyear.html)
+function readVariables(){
+	
+	// nav bar (except main pages, wgemail, and external links/random)
+	index = 'Housemoon Sprinter and friends welcome thou!';
+	intro = 'What an introduction!';
+	toons = 'Fun in thy blommin\' Dining Room!';
+	games = 'Housemoon Sprinter for Nintendo 64ii!';
+	characters = 'Super Housemoon Bros Melee was so much better than Super Housemoon Bros Brawl.';
+	housester = 'Housester 5.0';
+	comics = 'Read \'em up before you turn into a Goomba!';
+	email = 'Email Menu: Holy Crap! It\'s got multiple Emails! DAMN!';
+	updates = 'Up the date y\'all!';
+	support = 'Give us money, so we can give you not money!';
+	legal = 'Privacy Policy and Terms and Stuff... WHY ARE YOU READING THIS?! (Unless you\'re a legal official then you\'re forgiven.)';
+	legaltxt = 'Read the legal! In all its boring and tedious quality!';
+	wiimenumanual = 'Wii Menu';
+	wallpaper = 'Get Your Wallpapers! Free for Eternity!';
+	hmclock = 'And Bully\'s Special Prize! Keep the time and be mortified, it\'s a clock with a fellow\'s head on it!';
+	faq = 'Ask questions, maybe get answers! Emphasis on maybe!';
+	faq_mariopaint = 'Nice actual cartridge!';
+	museum = 'Repeating the same title isn\'t funny, ya douche - Microsoft Internet Explorer';
+	noflash = 'No Flash. No Ruffle. NO SERVICE!';
+	
+	// main pages
+	main1 = 'Housemoon is such a great tourist!';
+	main2 = 'Blaaarrrr Housemoon\'s a zombieeeeee...';
+	main3 = 'Happy Irish and English People getting drunk for "Saint Patrick" Day!';
+	main4 = 'Hey! Where\'s my light, mate? I can\'t see...';
+	main5 = 'Everybody loves the Housemoon Sprinter!';
+	main6 = 'You expecting this title to have anything of value? Ha! Give me a break...';
+	main7 = 'Meeple Moon, now on CBBC!';
+	main8 = 'Poor old Weak Good, can\'t even draw...';
+	main9 = 'Behind the Scenes at the Housemoon Sprinter Compound!';
+	main10 = 'The METAness has amazed nobody, more META than Facebook though.';
+	main11 = 'Not enough ash has been ingested yet.';
+	main12 = 'Every woodland creature gathers in a cave and plays the cello.';
+	lomsariitheprequel = 'Lomsar II: The Prequel coming to drug stoners and stores near your nostril!';
+	secretmain = 'Oh my weenis, its a secret!';
+	
+	// toons (this list is so BIG, it needs it's own sections)
+		// big toons
+		originalbook = 'The Fastest Sprinter in the World Contest';
+		housemoonsmarshmallowtrouble = 'Oh, Balloon Pop! I wasn\'t eating your cartoon! No, No, No!';
+		weakgoodcartoon = 'Weak Good loves newspapers!';
+		newcartoon = 'Wow! A new cartoon!';
+		_2ndyear = 'Housemoon Sprinter celebrates the Second Anniversary... CONTEST!!';
+		corona = 'Epidemics can be truly worrying, good grief...';
+		backagain = 'We\'re Back Again! Again. Now we\'re here and there\'s no need remembering when!';
+		housecleaning = 'Clean, clean, clean that urn!';
+		
+		// shorties
+		anvilcrush = 'Old timey, classic fun!';
+		orderhotdog = 'ORDERA HOTDOG! ORDERA HOTDOG! That won\'t get annoying, at all.';
+		maintenance = 'Under Maintenance!';
+		potenialintro = 'It truly doesn\'t matter... In this day and age...';
+		meeples = 'What a wonderous meeple collection you got there!';
+		mariopaintvid = '\'member when Housemoon used to like Marshmellows?';
+		aolcommercial = 'American Online, It\'s poop, It\'s crap...';
+		loading = 'When will it load? I don\'t know!';
+		dvdadvertise = 'This an advertisement and not a funny cartoon...';
+		macvspc = 'Macs have no viruses, but PCs have more programs.';
+		coppacartoon = 'Children\'s Online Privacy Protection Act, my buttox.';
+		chromeerror = 'I think your weeb rowser is broke to chunks';
+		mx2004toon = 'So old, it ain\'t cool anymore.';
+		hsvalve = 'Hazardous Valves are hazardous. Duh...';
+		fatmoonsprinter = 'The Fattest Man, in the Wooorld!';
+		toontastic = 'Listen to it and weep, if you can hear over the D E F A U L T music.';
+		venturebros = 'I\'m Enrico Matassa, latin playboy and insane egomaniac! My name means Hank Hank! And I\'m also using a hank of yarn as a weapon, also this is a huge hanky!'
+		flashtutorial = 'Made for a friend, given to you!';
+		flash2 = 'Animated in the earliest version of Flash! Ew...';
+		
+		// holidays
+		ween18_teaser = 'Can you guess the costumes??!';
+		ween18 = 'The judging of the coooooooosssssstuuuummmmeeeesssss!!!!!';
+		fireworks = 'Happy Bonfire Night/Fireworks Night/Guy Falkes Night!';
+		newyears2019 = 'Happy Late New Years! (Like 2 or 3 Months Late...)';
+		nationalpiday = 'Pi or Pie? Find out on this exciting episode of Housemoon Sprinter!';
+		mothersday = 'Happy NOT LATE Mothers Day! Sweet Gravy Lord!';
+		aprilfools2019 = 'R.I.P Housemoon Sprinter...';
+		ween19_teaser = 'Wait. Where is everyone?';
+		ween19 = 'Here comes Brexitoween! A day late but eh... Who cares?';
+		xmasshort1 = '3 Days till Christmas!';
+		newyears2020 = 'Not as late as last year!!';
+		houseflix = '"The newest wave in streaming technology!"';
+			// sprinmin housetor
+			sprinminhousetor = 'Housemoon Sprinter Presents: Sprinmin Housetor';
+			sprinminabode = 'Housemoon Sprinter Presents: Sprinmin Houseter - Adojo with his Sluts';
+			sprinminweakgood = 'Housemoon Sprinter Presents: Sprinmin Housetor - Weak Git at the Bar';
+		ween22_teaser = 'I know I keep mentioning the THREE years thing, but DAMN THREE YEARS?!';
+		queensjubilee = 'This was actually submitted to the BBC! No jokes for once!';
+		_5yearsteaser = 'Here Housemoon comes to wreck the daaaAAAAAAAaaay!';
+		newyears2024 = 'Get your best top hat, Get your magic yellow bag!';
+		
+		// powered by weak good
+		housemoondances = 'Are we in a circus or what?';
+		punches = 'Pow Back Burgh! Bligh!';
+		
+		// music videos
+		sugarsugar = 'Sugar, SHOOOOGGGAAARRRR!!!!!';
+		introducingthemeatles = 'Introducing, the Fab Four, the four only... THE MEATLES!';
+		meatlesonthemarket = 'The Meatles seem to be experimenting again...';
+		strawberryfields = 'Teen Meatle Squad!';
+		newyork = 'Numba Two, Top Ten!!';
+		christmasrevolution = 'You say you want an avant-grade song? Weeelll, you knoooow. We all want to shut you up.';
+		hawaiiparti = 'Think of these thoughts as limitless, limitless...';
+		
+		// wgemail (each computer will have its own title that is shared across each email)
+		wgemail = 'cWeak\'s Menu';
+		lappo434 = 'Lappo 434!';
+		cmac = 'Cresent\'s 27.5 inch outdated cMac!';
+		// wgemail - special cases
+		wgemail5 = 'Derive, Derive! Look who\'s Five!';
+		wgemail6 = 'Cresent\'s 27.5 inch cMac!';
+		wgemail10b = 'WGEmail 10: The Game, The Movie, The Broadway Show.';
+		// wgemail10a
+		wgemail10a = 'Getting more outdated by- Woah. Everyhting feels broken...';
+		
+		// hmemail
+		hmemail = 'Termin\'s Menu';
+		creasant = 'Cresent 5000!';
+		terminal = 'Terminate the Terminal!';
+		
+		// housemoonreviews
+		housemoonreviews = 'Housemoon Sprinter Reviews!';
+		
+		// warioware
+		warioware = 'Legitimate business! No cheat! ONLY WARIO.';
+		warioware1 = 'Housemoon I';
+		warioware2 = 'Housemoon II';
+		warioware3 = 'Housemoon III';
+		warioware4 = 'Housemoon IV';
+		warioware5 = 'Housemoon V';
+		warioware6 = 'Housemoon VI';
+		warioware7 = 'Housemoon VII';
+		warioware8 = 'Housemoon VIII';
+		warioware9 = 'Housemoon IX';
+		
+		// scrapbook
+		scrapbook = 'Check out this scrapped junk!';
+		exploringtheinternet = 'TOO BAD EARIO TIME.';
+		housemoonvirus = 'Housemoon, a fan of Macs? Since when?';
+		wherethecraparewe = 'Weak Good was mad, he knew he\'d been had. So he shot at Birmingham with a gun...';
+		wwcharactervid = 'Smile for the cameras, Weak Wad!';
+		hmemailization = 'The new and improved way to get Hmemails!';
+		hiatustoon = 'Everyone Loves... Housemoon Sprinter?';
+		internettoon_storyboard = 'Storyboards a plenty! Good as Irish Guiness!';
+		internettoon = 'Totally the reason why the site\'s been abandoned. Yeah, totally.';
+		meeths = 'At the end of the day, long as there\'s two people left on the planet, someone is gonna want to spit in a bucket.';
+		oldwgemail7 = 'Cresent\'s Dark mode cMac!';
+		yugidoh = 'Believe in the Heart of the Cards, and they shall belive in you...';
+		macvspc2 = 'The sequel NOBODY asked for!';
+		google = 'Google';
+		mrterminal = 'He\'s the man, the legend, Mr. Terminal!';
+		ween22 = 'Many found dead due to supposed "beef jerky violation".';
+		_5years = 'Five years, 60 months, 260 weeks, or 1,825 days—however you count it, it all adds up to toilet seats.';
+		
+		
+	// finally done with all the toons. YAY!
+		
+	// games
+	dance = 'Dance with Housemoon in this utter travastry of a game!';
+	housemoontetris = 'What a Tetromino!';
+	bubbles = 'Don\'t we all like to bubble?';
+	hsgb = 'Housemoon Sprinter GB: In all its puke green glory!';
+	wario0 = 'WARIO: 0 IT\'S ON ATARI DAMMIT!';
+	wgemailgame = 'One man gets stuck in a computer, Bullwinkle sues.';
+	
+	// blog pages
+	blogmeeples = 'What in the meeple?';
+	blogpeaceclasses = 'At peace with whimself!';
+	blogsweetvinyl = 'SUPER COOL YINYL - SPECIAL LIMITED EDITION (IN GREEN) - NOT CLICKBAIT!!!!';
+	blogchineseinterface = 'Google Translate is your friend!';
+	blogradofingame = 'Radofin is the best company that should\'ve been called Radotin!';
+	blogipadphoto = 'Get yer iPads \'ere! Wog to Laxmundham if yer want it!';
+	bloghousemoon5years = 'Seven Housemoons sitting on the wall, And if one Housemoon should accidentally fall, There\'ll be six Housemoons sitting on the wall.';
+	
+	// special pages (easter eggs or other pages)
+	pageisgoner = 'Uh Oh, your page is bloody gone!';
+	fcdbny = 'The Federal Codependeny Decency Bureau of New York is here to seize your ass!';
+	housecast = 'Housemoon Sprinter Housecast! Available at your discretion!';
+	originalhousecast = 'Housemoon Sprinter Housecast: "It\'s Cast... TASTIC!"';
+	
+	// other variables
+	isannouncement = true;
+	wallpaperpopup = false;
+	popupwidth = "0px";
+	popupheight = "0px";
+	popupheightadjustment = "0";
+	hsgbwidth = "0px";
+	hsgbheight = "0px";
+}
+
+// generates the page title
+function titleGenerator(pageurl){
+	
+	// here, we define where the hell the head is. So sitestuff.js knows where to dump its... well stuff. and specialtitle is the variable for telling the " - Housemoon Sprinter" part to bugger off, only a select few titles should have this condition.
+	var head = document.getElementsByTagName('head')[0];
+	var specialtitle = false;
+	
+	// now we're sure the head exists, read the title variable and create the title element
+	var titlepart1 = pageurl;
+	var titlegen = document.createElement('title');
+	console.trace(pageurl);
+	if(pageurl == museum || pageurl == google){
+		specialtitle = true;
+	}
+	else{
+		specialtitle = false;
+	}
+	
+	// yet again another fallback, to make sure everything still looks alright.
+	if(titlepart1 == undefined){
+		titlegen.innerHTML = 'No Title Defined - Housemoon Sprinter';
+		console.trace('pageurl has not been defined, falling back');
+	}
+	if(specialtitle == true){
+		titlegen.innerHTML = titlepart1;
+		console.trace('special pageurl detected');
+	}
+	// change the "RHMFSE" to the name of your site. Gives a bit of identity to your pages and helps with SEO.
+	else{
+		titlegen.innerHTML = titlepart1 + " - Housemoon Sprinter";
+	}
+	// add the title already script.
+	head.appendChild(titlegen);
+}
+
+// allows buttons to work for the No Flash Page, yes they're old Macromedia scripts. Why do you care?
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+
+// Preloads the images to save on loading times.
+MM_preloadImages('/noflash/ruffle-button_o.png');
+MM_preloadImages('/noflash/flash-button_o.png');
